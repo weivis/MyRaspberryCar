33ce2d0170e4b34fa5877be277d95bd252b00faf
ShenVi <yvban@outlook.com>
2019-08-17 06:30:15 +0800

    1

--- apps.py
+++ apps.py
@@ -1,6 +1,28 @@
 # coding:utf-8
 import RPi.GPIO as GPIO
 import time
+#绑定对应的引脚，来自于图纸
+PWMA=18
+AIN1 = 22
+AIN2 = 27
+PWMB=23
+BIN1= 25
+BIN2 = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+# 设置引脚为输出
+GPIO.setup(PWMA, GPIO.OUT)
+GPIO.setup(AIN1, GPIO.OUT)
+GPIO.setup(AIN2, GPIO.OUT)
+GPIO.setup(PWMB, GPIO.OUT)
+GPIO.setup(BIN1, GPIO.OUT)
+GPIO.setup(BIN2, GPIO.OUT)
+#电机
+leftMotor = GPIO.PWM(PWMA, 100)
+rightMotor = GPIO.PWM(PWMB, 100)
+leftMotor.start(0)
+rightMotor.start(0)
 
 def forward(speed,runtime):
     leftMotor.ChangeDutyCycle(speed)
