ffe97674ef591048940f58190c2a69576df03dc7
ShenVi <yvban@outlook.com>
2019-08-18 01:01:09 +0800

    Update apps.py

--- apps.py
+++ apps.py
@@ -30,7 +30,8 @@ rightMotor = GPIO.PWM(PWMB, 100)
 leftMotor.start(0)
 rightMotor.start(0)
 
-def runbody(speed, backtime):
+def forward(speed, backtime):
+    print('前进')
     leftMotor.ChangeDutyCycle(speed)
     GPIO.output(AIN2, True)  # AIN2
     GPIO.output(AIN1, False)  # AIN1
@@ -39,9 +40,44 @@ def runbody(speed, backtime):
     GPIO.output(BIN1, False)  # BIN1
     time.sleep(backtime)
 
+
+def backword(speed, backtime):
+    print('后退')
+    leftMotor.ChangeDutyCycle(speed)
+    GPIO.output(AIN2, True)  # AIN2
+    GPIO.output(AIN1, False)  # AIN1
+    rightMotor.ChangeDutyCycle(speed)
+    GPIO.output(BIN2, True)  # BIN2
+    GPIO.output(BIN1, False)  # BIN1
+    time.sleep(backtime)
+
+#左转弯函数
+def turnLeft(speed, lefttime):
+    print('左转向')
+    leftMotor.ChangeDutyCycle(speed)
+    GPIO.output(AIN1, False)
+    GPIO.output(AIN2, True)
+    rightMotor.ChangeDutyCycle(speed)
+    GPIO.output(BIN1, True)
+    GPIO.output(BIN2, False)
+    time.sleep(lefttime)
+
+def turnRight(speed, righttime):
+    print('右转向')
+    leftMotor.ChangeDutyCycle(speed)
+    GPIO.output(AIN1, True)
+    GPIO.output(AIN2, False)
+    rightMotor.ChangeDutyCycle(speed)
+    GPIO.output(BIN1, False)
+    GPIO.output(BIN2, True)
+    time.sleep(righttime)
+
 if __name__ == '__main__':
     try:
          while True:
-             runbody(50,3)
+             forward(50,3)
+             turnLeft(70, 3)
+             backword(50,3)
+             turnRight(70,3)
     except KeyboardInterrupt:
          GPIO.cleanup()
