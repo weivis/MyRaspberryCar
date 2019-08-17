38ed143e2fccd23eff243304090c1f71bbcd0efe
ShenVi <yvban@outlook.com>
2019-08-18 00:10:37 +0800

    电机

--- apps.py
+++ apps.py
@@ -1,17 +1,24 @@
 # coding:utf-8
 import RPi.GPIO as GPIO
 import time
-#绑定对应的引脚，来自于图纸
-PWMA=18
+
+GPIO.cleanup()
+
+'''18引脚控制的是左侧两个电机'''
+PWMA = 18
+'''23引脚控制的右侧两个电机'''
+PWMB = 23
+
+'''AIN1、AIN2、BIN1、BIN2控制的是左右两侧电机的正转和翻转'''
 AIN1 = 22
 AIN2 = 27
-PWMB=23
-BIN1= 25
+BIN1 = 25
 BIN2 = 24
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+'''设置引脚为输出'''
 GPIO.setup(PWMA, GPIO.OUT)
 GPIO.setup(AIN1, GPIO.OUT)
 GPIO.setup(AIN2, GPIO.OUT)
@@ -19,18 +26,20 @@ GPIO.setup(PWMB, GPIO.OUT)
 GPIO.setup(BIN1, GPIO.OUT)
 GPIO.setup(BIN2, GPIO.OUT)
 
+#电机
 leftMotor = GPIO.PWM(PWMA, 100)
 rightMotor = GPIO.PWM(PWMB, 100)
 leftMotor.start(0)
 rightMotor.start(0)
 
-def forward(speed,runtime):
-    leftMotor.ChangeDutyCycle(speed)
-    GPIO.output(AIN1, True)#AIN1高电平则正转
-    # GPIO.output(AIN2, True)#如果为True则翻转
-    # rightMotor.ChangeDutyCycle(speed)
-    # GPIO.output(BIN1, False)
-    # GPIO.output(BIN2, False)
-    time.sleep(runtime)
+# 电机旋转速度
+leftMotor.ChangeDutyCycle(50)
+rightMotor.ChangeDutyCycle(50)
 
-forward(80,20)
+GPIO.output(AIN1, True)
+GPIO.output(AIN2, True)
+GPIO.output(BIN1, True)
+GPIO.output(BIN2, True)
+time.sleep(3)
+
+GPIO.cleanup()
